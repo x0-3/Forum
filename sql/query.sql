c805d2853715c42abae90cd2154545c4e89f6407
x0-3 <steph6363@gmail.com>
2023-03-21 19:51:09 +0100

    uml modified and query needed for layout in sql folder

---
+++ sql/query.sql
@@ -0,0 +1,49 @@
+-- Home page 
+
+SELECT id_topic, id_user, title, DATE_FORMAT(topicCreatedAt,'%d/%m/%Y') AS DATE, avatar, pseudo
+FROM topic t
+INNER JOIN user u
+ON t.user_id = u.id_user
+ORDER BY YEAR(DATE) DESC, MONTH(DATE) DESC 
+
+-- category page
+-- show categories on the page
+SELECT nameCategory
+FROM category
+
+-- redirect for topic page 
+SELECT id_category, id_topic, title
+FROM category c
+INNER JOIN topic t
+ON t.category_id = c.id_category
+WHERE id_category = 3
+
+
+-- topic page
+SELECT id_topic, id_user, title, DATE_FORMAT(topicCreatedAt,'%d/%m/%Y') AS DATE, avatar, pseudo
+FROM topic t
+INNER JOIN user u
+ON t.user_id = u.id_user
+ORDER BY YEAR(DATE) DESC, MONTH(DATE) DESC 
+
+
+-- article page
+
+-- article section
+SELECT id_topic, id_user, title, DATE_FORMAT(topicCreatedAt,'%d/%m/%Y') AS DATE, avatar, pseudo, lockTopic
+FROM topic t
+INNER JOIN user u
+ON t.user_id = u.id_user
+WHERE id_topic = 2
+
+-- messages section
+SELECT id_message, id_topic, id_user, pseudo, DATE_FORMAT(messCreatedAt,'%d/%m/%Y') AS commentDate, text
+FROM message m
+INNER JOIN topic t
+ON m.topic_id = t.id_topic
+INNER JOIN user u
+ON u.id_user = m.user_id
+WHERE id_topic = 2
+ORDER BY YEAR(commentDate) DESC, MONTH(commentDate) DESC 
+
+
