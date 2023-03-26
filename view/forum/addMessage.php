60c172ce34cbce49994f09088fa0212e12930175
x0-3 <steph6363@gmail.com>
2023-03-26 15:20:52 +0200

    fix add message Uncaught TypeError: array_keys()

--- view/forum/addMessage.php
+++ view/forum/addMessage.php
@@ -1,13 +1,17 @@
 <!-- // FIXME:add an insert into look into the syntaxe -->
-
 <?php
 $message = ["data"]["messages"];
+
+$data = $_POST['text'];
+
+filter_input(INPUT_POST,"text",FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 ?>
+
 
 <section class="addComment">
     <h1>Add a comment for</h1>
 
     <form action="post">
-        <input type="text" value="TEXT" name="text" id="text" placeholder="What do you want to write about ...">
+        <input type="text" value="text" name="text" id="text" placeholder="What do you want to write about ...">
     </form>
 </section>
